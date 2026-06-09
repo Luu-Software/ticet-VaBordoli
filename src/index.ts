1927f582fd3bf86c7c71a3198e7923d915f3a804
VaBordoli <valentino.bordoli@gmail.com>
2026-06-09 15:48:52 -0300

    TERMINO ACTIVIDAD

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,39 @@ DARIO       |       50%
 
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
 
+function calcularTotal(id:string, cantidad:number, codigo:string): number{
+let precio: number = 0;
+if (id === "sabrina"){
+  precio = 1000
+} else if (id === "kgatlw"){
+ precio = 700
+} else if (id === "lali"){
+  precio = 500
+} else if (id === "magdalena"){
+  precio = 600
+} else if (id === "viagra"){
+  precio = 400
+} else if (id === "dillom"){
+  precio = 350
+} else if (id === "marilina"){
+  precio = 200
+} else if (id === "mugre"){
+  precio = 150
+} else {
+  precio = 0
+}
+if (codigo === "TIC10"){
+  precio  = precio*0.9
+}else if(codigo === "TIC20"){
+  precio = precio*0.8
+}else if(codigo === "DARIO"){
+  precio = precio*0.5
+}
+
+return precio*cantidad;
+  
+}
+
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
   let cantidadNum: number = Number(cantidad);
   let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
